4e153b1d15204f0b89484fa5cc4c362f7332901c
mamniash <mamniashvili2003@gmail.com>
2024-10-23 21:09:37 +0300

    fix img in build mode

--- src/data/productList.ts
+++ src/data/productList.ts
@@ -1,45 +1,49 @@
+import headphones1 from '../assets/headphones1.png'
+import headphones2 from '../assets/headphones2.png'
+import headphones3 from '../assets/headphones3.png'
+
 import { ProductType } from '../components/common/Product'
 
 export const productList: ProductType[] = [
 	{
 		title: 'Read Headphone',
 		grade: '4.70',
-		img: '/src/assets/headphones1.png',
+		img: headphones1,
 		color: '#FFE5EE',
 		price: '256'
 	},
 	{
 		title: 'Blue Headphone',
 		grade: '4.90',
-		img: '/src/assets/headphones2.png',
+		img: headphones2,
 		color: '#E5F1FF',
 		price: '235'
 	},
 	{
 		title: 'Green Headphone',
 		grade: '4.80',
-		img: '/src/assets/headphones3.png',
+		img: headphones3,
 		color: '#E5FFFB',
 		price: '245'
 	},
 	{
 		title: 'Read Headphone',
 		grade: '4.70',
-		img: '/src/assets/headphones1.png',
+		img: headphones1,
 		color: '#FFE5EE',
 		price: '256'
 	},
 	{
 		title: 'Blue Headphone',
 		grade: '4.90',
-		img: '/src/assets/headphones2.png',
+		img: headphones2,
 		color: '#E5F1FF',
 		price: '235'
 	},
 	{
 		title: 'Green Headphone',
 		grade: '4.80',
-		img: '/src/assets/headphones3.png',
+		img: headphones3,
 		color: '#E5FFFB',
 		price: '245'
 	}
