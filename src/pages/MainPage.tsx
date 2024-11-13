4bb66f19966ec108efd1e63c4c921283b827f777
mamniash <mamniashvili2003@gmail.com>
2024-11-13 22:21:17 +0300

    fix build

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import ProductList from '../components/common/ProductList'
 import WhatInTheBox from '../components/common/WhatInTheBox'
 import Order from '../components/common/Order'
